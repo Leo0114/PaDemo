35655699ba52d49210a16543d809a11391e4f055
Leo0114 <leonel0614@gmail.com>
2023-09-14 01:06:15 -0600

    2stDeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://Leo0114.github.io",
   base: "/PaDemo",
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()]
 });
