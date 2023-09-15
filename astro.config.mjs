843de630d89e475ec87e5b25273a66e1a3847965
Leo0114 <leonel0614@gmail.com>
2023-09-14 19:01:56 -0600

    fligh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  image: { service: squooshImageService() },
   site: "https://Leo0114.github.io",
   base: "/PaDemo",
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact()],
 });
