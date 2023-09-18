887eb009fd6121b01769df1e8ba21c2e8c9a542d
Leo0114 <leonel0614@gmail.com>
2023-09-17 23:58:01 -0600

    BgChanges

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,20 @@ const boardCollection = defineCollection({
     }),
 });
 
+const weeksCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      dia: z.string(),
+      cover: image(),
+      coverAlt: z.string(),
+      encargado: z.string(),
+      correoEncargado: z.string(),
+    }),
+});
+
 export const collections = {
   about: boardCollection,
+  weeks: weeksCollection,
 };
