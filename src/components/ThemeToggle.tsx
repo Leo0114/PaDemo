7ca7ede1e8a9e91fc506ec957815720b0d643345
Leo0114 <leonel0614@gmail.com>
2023-09-18 19:41:58 -0600

    navchange

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -22,10 +22,10 @@ export default function ThemeToggle() {
     return (
       <button
         onClick={handleClick}
-        className=" mx-auto flex-row justify-center dark:text-slate-200 text-slate-950  transition-all cursor-pointer hover:bg-slate-300 dark:hover:bg-slate-700
+        className=" flex-row justify-center dark:text-slate-100 text-slate-950  transition-all cursor-pointer hover:bg-slate-300 dark:hover:bg-slate-700
         focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg
          px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55
-          mr-2 mb2 hover:shadow-lg  duration-300 ease-in-out hover:scale-110 scale-90 gap-x-2 opacity-70 hover:opacity-100"
+          mr-2 mb2 hover:shadow-lg  duration-300 ease-in-out hover:scale-110 scale-90 gap-x-2 opacity-70 hover:opacity-100 mx-auto"
         aria-label="Modo oscuro"
       >
         {theme === "light" ? <MoonSht /> : <DarkSh />}
