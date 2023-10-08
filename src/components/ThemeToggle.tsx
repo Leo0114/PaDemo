e44ec04294d4ead892e04679068f98efe325fd2e
Leo0114 <leonel0614@gmail.com>
2023-10-08 02:24:15 -0600

    small changes

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -22,10 +22,7 @@ export default function ThemeToggle() {
     return (
       <button
         onClick={handleClick}
-        className=" flex-row justify-center dark:text-slate-100 text-slate-950  transition-all cursor-pointer hover:bg-slate-300 dark:hover:bg-slate-700
-        focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg
-         px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55
-          mr-2 mb2 hover:shadow-lg  duration-300 ease-in-out hover:scale-110 scale-90 gap-x-2 opacity-70 hover:opacity-100 mx-auto"
+        className="  mx-auto group relative flex h-9 w-9 rounded-full transition duration-500 hover:bg-slate-950/5 dark:hover:bg-white/10"
         aria-label="Modo oscuro"
       >
         {theme === "light" ? <MoonSht /> : <DarkSh />}
--- src/components/layout/LosIconos.tsx
+++ src/components/layout/LosIconos.tsx
@@ -1,17 +1,13 @@
 function DarkSh() {
   return (
     <svg
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6 "
+      aria-hidden="true"
+      class="transition absolute inset-0 m-auto h-5 w-5 rotate-0 scale-0 fill-white/70 opacity-0 duration-500 group-hover:opacity-100 dark:order-1 dark:rotate-180 dark:scale-100 dark:opacity-100 dark:group-hover:fill-yellow-400"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 16 16"
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-      />
+      <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"></path>
     </svg>
   );
 }
@@ -19,17 +15,14 @@ function DarkSh() {
 const MoonSht = () => {
   return (
     <svg
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
+      aria-hidden="true"
+      class="transition absolute inset-0 m-auto h-5 w-5 rotate-0 scale-95 fill-gray-500 duration-500 group-hover:fill-blue-950 dark:-rotate-180 dark:scale-0 dark:opacity-0 dark:group-hover:fill-white"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 16 16"
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-      />
+      <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"></path>
+      <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"></path>
     </svg>
   );
 };
